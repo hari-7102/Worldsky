757b98122970a98dda605d91e3ce973ec98574e0
Hariharan S <130534328+hari-7102@users.noreply.github.com>
2026-03-27 18:54:21 +0530

    Add new line in App component

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import Japan from './component/Destination/Japan'
 import Italy from './component/Destination/Italy'
 import India from './component/Destination/India'
 const App = () => {
+  
 
 
   return (
